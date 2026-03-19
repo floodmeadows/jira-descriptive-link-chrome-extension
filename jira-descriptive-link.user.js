4fea03c74e15369557d3b414e77536cfb7ea2b5f
Andy Vaughan <20927751+floodmeadows@users.noreply.github.com>
2026-03-19 22:31:52 +0000

    Update manifest file and script version number to 1.1. Update to latest version of script that uses mutation observer to re-add the button on partial page content refreshes that remove the button.

--- jira-descriptive-link.user.js
+++ jira-descriptive-link.user.js
@@ -2,10 +2,11 @@
 // @name         Jira descriptive link
 // @namespace    https://openuserjs.org/users/floodmeadows
 // @description  Adds a copyable link to issue details, for pasting into messages etc.
-// @copyright    2023, floodmeadows (https://openuserjs.org/users/floodmeadows)
+// @copyright    2026, floodmeadows (https://openuserjs.org/users/floodmeadows)
 // @license      MIT
-// @version      0.3
-// @include      https://jira.*.uk/browse/*
+// @version      1.1
+// @include      https://*/browse/*
+// @include      https://*/projects/*
 // @grant        none
 // ==/UserScript==
 
@@ -13,40 +14,51 @@
 // @author       floodmeadows
 // ==/OpenUserJS==
 
-/* jshint esversion: 6 */
+/* jshint esversion: 11 */
 
 (function () {
   'use strict';
 
-  addLink();
+  // -----------------------------
+  // CONFIGURATION
+  // -----------------------------
+  const markerId = 'copy-descriptive-link-button-added';
+  let issueKey = "";
+  let issueName = "";
+  let issueUrl = "";
 
-  const target = document.getElementById('key-val').parentElement.parentElement; //<ol>
-  addButton("Copy descriptive link", target, copyTextToClipboard);
-  applyAdditionalFormatting();
-})();
+  // -----------------------------
+  // INITIALISE
+  // -----------------------------
+  function init() {
+    issueKey = document.getElementById('key-val')?.textContent.trim();
+    issueName = getIssueName();
+    issueUrl = document.URL;
+    checkIfButtonNeedsAdding();
+  }
 
-function addLink() {
-const issueKey = document.getElementById("key-val").childNodes[0].nodeValue;
-const issueName = document.getElementById("summary-val").childNodes[0].nodeValue;
-const newElement = document.createElement("div");
-const h = '<a href="' + document.URL + '">\'' + issueName + '\' (' + issueKey + ')</a>';
-newElement.innerHTML = h;
-const target = document.getElementById('summary-val');
-target.parentNode.appendChild(newElement);
+  function checkIfButtonNeedsAdding() {
+    console.log("start called")
+    setTimeout(() => {
+      const target = document.getElementById('key-val').parentElement.parentElement; //<ol>
+      if (!target) {
+        console.log("Target element not found. Can't add button.")
+        return
+      }
+      if (document.getElementById(markerId)) {
+        console.log("Marker element not found. Returning without adding button.")
+        return;
+      }
+      console.log("Injecting button...");
+      const marker = document.createElement('span');
+      marker.id = markerId;
+      target.after(marker);
+      addButton(target, "Copy descriptive link", copyTextToClipboard);
+      applyAdditionalFormatting();
+    }, 500); // delay to allow DOM to settle
 }
 
-function constructTextForClipboard() {
-  const issueKey = document.getElementById("key-val").childNodes[0].nodeValue;
-  const issueName = document.getElementById("summary-val").childNodes[0].nodeValue;
-
-  var textForClipboard = new Object();
-  textForClipboard.text = `'${issueName}' (${document.URL})`;
-  textForClipboard.html = `<a href="${document.URL}">'${issueName}' (${issueKey})</a>`;
-
-  return textForClipboard;
-}
-
-function addButton(buttonText, target, callbackFunction) {
+  function addButton(target, buttonText, callbackFunction) {
   const button = document.createElement("button");
   button.addEventListener("click", callbackFunction);
   button.setAttribute("id","copy-descriptive-link");
@@ -62,6 +74,31 @@ function applyAdditionalFormatting() {
   document.getElementById('copy-descriptive-link').style.display = "inline-block";
   document.getElementById('copy-descriptive-link').style.border = "none";
   document.getElementById('copy-descriptive-link').style.padding = "0.4em 0.8em";
+ }
+
+  function getIssueName() {
+    let issueNameH1 = document.querySelector('h1#summary-val')
+    let issueNameDiv = document.querySelector('div#summary-val')
+    let issueNameH2 = document.querySelector('h2#summary-val')
+    let issueName = ''
+    if (issueNameH1 !== null) {
+        issueName = issueNameH1.childNodes[0].nodeValue;
+    } else if (issueNameDiv !== null) {
+        issueName = issueNameDiv.childNodes[0].childNodes[0].nodeValue
+    } else if (issueNameH2 !== null) {
+        issueName = issueNameH2.childNodes[0].nodeValue
+    }
+
+    return issueName
+  }
+
+
+  function constructTextForClipboard() {
+    var textForClipboard = new Object();
+    textForClipboard.text = `'${issueName}' (${issueUrl})`;
+    textForClipboard.html = `<a href="${issueUrl}">'${issueName}' (${issueKey})</a>`;
+
+    return textForClipboard;
 }
 
 function copyTextToClipboard() {
@@ -90,3 +127,19 @@ function copyTextToClipboard() {
           }
       );
 }
+
+  // -----------------------------
+  // OBSERVER TO HANDLE DOM CHANGES
+  // -----------------------------
+
+  const observer = new MutationObserver((mutationsList, observer) => {
+    console.log("DOM changed, checking buttons...");
+    checkIfButtonNeedsAdding();
+  });
+  observer.observe(document.body, { childList: true, subtree: true });
+  window.__observerActive = true;
+  console.log("MutationObserver started");
+
+  // Run on initial load
+  init();
+})();
